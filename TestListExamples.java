2a22e4e30f0314eb255cefe7d214872992db80ac
yuribling123 <122572429+yuribling123@users.noreply.github.com>
2023-02-17 19:12:49 -0500

    c

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,4 +20,20 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+  public void testCheckFilter() {
+  // Returns a new list that has all the elements of the input list for which
+  // the StringChecker returns true, and not the elements that return false, in
+  // the same order they appeared in the input list;
+  // List<String> filter(List<String> list, StringChecker sc)
+  List<String> mylist= new ArrayList<>();
+  StringChecker sc =new IsMoon();
+  mylist.add("moon");
+  List<String> result=ListExamples.filter(mylist,sc);
+  List<String> expect=new ArrayList<>();
+  expect.add("moon");
+    assertEquals(expect, result);
+  }
+  
 }
--- grade.sh
+++ grade.sh
@@ -1,5 +1,39 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar' #set the envirornment for junit
 
-rm -rf student-submission
-git clone $1 student-submission
+rm -rf student-submission #delete the exisiting submission
+git clone $1 student-submission #change the directory name to student-submission and clone
 echo 'Finished cloning'
+
+
+set -e
+if [[ -f $(find student-submission -name "ListExamples.java") ]]; then
+  echo "File Exist!!!"
+else
+  echo "File Does Not Exist!!!"
+  exit 1
+fi
+path=`find student-submission -name "ListExamples.java"`
+cp -n "$path" ./student-submission/
+
+cd ./student-submission
+set +e
+cp ../TestListExamples.java ./
+cp -r ../lib ./
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+if [ $? -ne 0 ]  #if the exit code is not 0, the file fails to compile
+then
+  echo "The File Does Not Compile!!!"
+   exit 1
+fi
+
+echo "The File Compiles!!!"
+# redirect the output of junit test to new file
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > result.txt
+
+if grep  "OK" result.txt ; then
+    echo "Score: Pass"
+else
+    echo "Score: Fail"
+fi
+
+exit 0
